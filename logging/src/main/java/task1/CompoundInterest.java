48a82b77aa478876db73b66cf317c2afc0e69442
Ginne Abhinaya Sri <60144898+ginnereddy@users.noreply.github.com>
2020-02-26 12:56:17 +0530

    Add files via upload

---
+++ logging/src/main/java/task1/CompoundInterest.java
@@ -0,0 +1,9 @@
+package task1;
+
+public class CompoundInterest {
+
+	double compoundInterestCalculator(float principle,float time,float rateOfInterest)
+	{
+		return (principle*Math.pow(1+(rateOfInterest/100),time));
+	}
+}
---
+++ logging/src/main/java/task1/SimpleInterest.java
@@ -0,0 +1,8 @@
+package task1;
+
+public class SimpleInterest {
+	float simpleInterestCalculator(float principle,float time,float rateOfInterest)
+	{
+		return ((principle*time*rateOfInterest)/100);
+	}
+}
